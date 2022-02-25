14b3b9df8eb7c53a36a9bbeee1a6fa42e9c128ef
Leo Cucinell <leocucinell@gmail.com>
2022-02-25 09:53:25 -1000

    finished reservation methods

--- controllers/ReservationController.js
+++ controllers/ReservationController.js
@@ -10,20 +10,88 @@ const retrieve_reservation = (req, res) => {
     res.send('Get Reservation');
 }
 
-const retrieve_user_reservations = (req, res) => {
-    res.send('List of reservations for a particular user')
+const retrieve_user_reservations = async (req, res) => {
+    try{
+        const userReservations = await prisma.customer.findUnique({
+            where: {
+                id: req.body.id
+            },
+            select: {
+                reservations: true
+            }
+        });
+        res.status(200).json({
+            message: 'Success',
+            data: userReservations
+        });
+
+    } catch(err) {
+        console.log(err)
+        res.status(400).json({
+            message: "Error retrieving user reservations"
+        });
+    }
 }
 
-const retrieve_restuarant_reservations = (req, res) => {
-    res.send('Restrieve list of restuarant reservations')
+const retrieve_restuarant_reservations = async (req, res) => {
+    try{
+        const userReservations = await prisma.restuarant.findUnique({
+            where: {
+                id: req.body.id
+            },
+            select: {
+                reservations: true
+            }
+        });
+        res.status(200).json({
+            message: 'Success',
+            data: userReservations
+        });
+
+    } catch(err) {
+        console.log(err)
+        res.status(400).json({
+            message: "Error retrieving user reservations"
+        });
+    }
 }
 
-const add_reservation = (req, res) => {
-    res.send('Add Reservation');
+const add_reservation = async (req, res) => {
+    try{
+        //create the reservation and set the owner_id & resturaunt_id
+        const createdReservation = await prisma.reservation.create({
+            data: {
+                startTime: req.body.startTime,
+                owner_id: req.body.owner_id,
+                restuarant_id: req.body.restuarant_id
+            }
+        });
+        res.status(200).json({
+            message: 'success',
+            data: createdReservation
+        });
+
+    } catch(err) {
+        console.log(err);
+        res.status(400).json({
+            message: 'Error adding reservation'
+        })
+    }
 }
 
-const delete_reservation = (req, res) => {
-    res.send('Delete Reservation');
+const delete_reservation = async (req, res) => {
+    try{
+        const deletedReservation = await prisma.reservation.delete({
+            where: {
+                id: req.body.id
+            }
+        });
+    } catch(err) {
+        console.log(err);
+        res.status(400).json({
+            message: 'Error deleting reservation'
+        })
+    }
 }
 
 //SECTION: Exports
